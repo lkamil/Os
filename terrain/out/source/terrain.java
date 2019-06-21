392bd911b520645b9df102e8a0867789b48c23e1
lkamil <lkamil@protonmail.com>
2019-06-21 14:50:26 +0200

    add camera + images

--- terrain/out/source/terrain.java
+++ terrain/out/source/terrain.java
@@ -17,15 +17,23 @@ public class terrain extends PApplet {
 // create a terrain instance
 Terrain t = new Terrain();
 
+// values for camera
+float centerY = height / 2;
+float centerZ = 100;
+float cameraAngle = 20.0f;
+
 public void setup() {
     // create a 3D canvas
     
+    // camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
+    camera(width/2, height, centerZ + centerY / tan(cameraAngle * PI / 180), width/2, centerY, centerZ, 0, 1, 0);
 }
 
 public void draw() {
     background(50);
-    positionCanvas();
+    // positionCanvas();
     t.display();
+    // t.displayHeightmap(); // comment out camera() in setup to draw heightmap correctly
 }
 
 public void positionCanvas() {
@@ -41,7 +49,7 @@ class Terrain {
     float[][] heightMap; // contains the height - or z value - for each vertice
 
     Terrain() {
-        w = 820;
+        w = 1000;
         h = 600;
         scl = 10;
         maxHeight = 200;
@@ -52,13 +60,6 @@ class Terrain {
         heightMap = new float[cols][rows];
 
         calculateZValues();
-
-        // // Set initial values
-        // for (int x = 0; x < cols; x++) {
-        //     for (int y = 0; y < rows; y++) {
-        //         heightMap[x][y] = 0;
-        //     }
-        // }
     }
 
     public void calculateZValues() {
@@ -77,16 +78,40 @@ class Terrain {
     }
 
     public void display() {
-        stroke(255);
+        //stroke(255);
         noFill();
 
         for (int y = 0; y < rows - 1; y++) {
+            if ( y < rows / 5) {
+                stroke(150);
+            } else if (y < rows / 4) {
+                stroke(180);
+            } else if (y < rows / 3) {
+                stroke(210);
+            } else if (y < rows / 2) {
+                stroke(235);
+            } else {
+                stroke(255);
+            }
+
+            int widthOffset = 100;
             beginShape(TRIANGLE_STRIP); // Build triangle strip row by row
             for (int x = 0; x < cols; x++) {
-                vertex(x * scl, y * scl, heightMap[x][y]);
-                vertex(x * scl, (y + 1) * scl, heightMap[x][y+1]);
+                vertex(x * scl - widthOffset, y * scl, heightMap[x][y]);
+                vertex(x * scl - widthOffset, (y + 1) * scl, heightMap[x][y+1]);
             }
             endShape();
+        }
+    }
+
+    public void displayHeightmap() {
+        noStroke();
+        for (int y = 0; y < rows - 1; y++) {
+            for (int x = 0; x < cols; x++) {
+                float c_val = map(heightMap[x][y], 0, maxHeight, 0, 255);
+                fill(c_val, c_val, c_val);
+                rect(x*scl, y*scl, scl, scl);
+            }
         }
     }
 }
--- terrain/terrain.pde
+++ terrain/terrain.pde
@@ -1,15 +1,23 @@
 // create a terrain instance
 Terrain t = new Terrain();
 
+// values for camera
+float centerY = height / 2;
+float centerZ = 100;
+float cameraAngle = 20.0;
+
 void setup() {
     // create a 3D canvas
     size(800, 600, P3D);
+    // camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
+    camera(width/2, height, centerZ + centerY / tan(cameraAngle * PI / 180), width/2, centerY, centerZ, 0, 1, 0);
 }
 
 void draw() {
     background(50);
-    positionCanvas();
+    // positionCanvas();
     t.display();
+    // t.displayHeightmap(); // comment out camera() in setup to draw heightmap correctly
 }
 
 void positionCanvas() {
@@ -25,7 +33,7 @@ class Terrain {
     float[][] heightMap; // contains the height - or z value - for each vertice
 
     Terrain() {
-        w = 820;
+        w = 1000;
         h = 600;
         scl = 10;
         maxHeight = 200;
@@ -54,16 +62,40 @@ class Terrain {
     }
 
     void display() {
-        stroke(255);
+        //stroke(255);
         noFill();
 
         for (int y = 0; y < rows - 1; y++) {
+            if ( y < rows / 5) {
+                stroke(150);
+            } else if (y < rows / 4) {
+                stroke(180);
+            } else if (y < rows / 3) {
+                stroke(210);
+            } else if (y < rows / 2) {
+                stroke(235);
+            } else {
+                stroke(255);
+            }
+
+            int widthOffset = 100;
             beginShape(TRIANGLE_STRIP); // Build triangle strip row by row
             for (int x = 0; x < cols; x++) {
-                vertex(x * scl, y * scl, heightMap[x][y]);
-                vertex(x * scl, (y + 1) * scl, heightMap[x][y+1]);
+                vertex(x * scl - widthOffset, y * scl, heightMap[x][y]);
+                vertex(x * scl - widthOffset, (y + 1) * scl, heightMap[x][y+1]);
             }
             endShape();
+        }
+    }
+
+    void displayHeightmap() {
+        noStroke();
+        for (int y = 0; y < rows - 1; y++) {
+            for (int x = 0; x < cols; x++) {
+                float c_val = map(heightMap[x][y], 0, maxHeight, 0, 255);
+                fill(c_val, c_val, c_val);
+                rect(x*scl, y*scl, scl, scl);
+            }
         }
     }
 }
