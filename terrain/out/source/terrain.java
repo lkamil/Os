d3b6c9ed02ad69604ac39970a94c8eca40b0db38
lkamil <lkamil@protonmail.com>
2019-06-23 15:29:36 +0200

    camera flies over the mountains

--- terrain/out/source/terrain.java
+++ terrain/out/source/terrain.java
@@ -21,13 +21,14 @@ Terrain t = new Terrain();
 float centerHeight = 100;
 float cameraAngle = 60.0f;
 float shift = 0; // for moving the camera to fly over the terrain
+float cameraY = 0;
 
 public void setup() {
     // create a 3D canvas
     
     // camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
     // camera(width/2, height, centerZ + centerY / tan(cameraAngle * PI / 180), width/2, centerY, centerZ, 0, 1, 0);
-    camera(width/2, 0, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2), centerHeight, 0, 1, 0);
+    camera(width/2, cameraY, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2), centerHeight, 0, 1, 0);
     
 }
 
@@ -42,9 +43,9 @@ public void draw() {
 }
 
 public void moveCamera() {
-    shift += 0.5f;
+    cameraY -= 2;
     //camera(width/2, height - shift, centerZ + centerY / tan(cameraAngle * PI / 180), width/2, centerY - shift, centerZ, 0, 1, 0);
-    camera(width/2, 0 - shift, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2) - shift, centerHeight, 0, 1, 0);
+    camera(width/2, cameraY, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2) + cameraY, centerHeight, 0, 1, 0);
 }
 
 public void positionCanvas() {
@@ -58,6 +59,9 @@ class Terrain {
     int w, h; // width and height - or better depth -  of the terrain
     int maxHeight; // defines the maximum height a mountain can have
     float[][] heightMap; // contains the height - or z value - for each vertice
+
+    float xoff = 0.0f;
+    float yoff = 0.0f;
 
     Terrain() {
         w = 1000;
@@ -74,9 +78,8 @@ class Terrain {
     }
 
     public void calculateZValues() {
-        float yoff = 0.0f;
         for (int y = 0; y < rows; y++) {
-            float xoff = 0.0f; // set xoff to 0 before traversing a new row!
+            xoff = 0.0f; // set xoff to 0 before traversing a new row!
             for (int x = 0; x < cols; x++) {
 
                 // set a z value for each element using perlin noise
@@ -90,21 +93,33 @@ class Terrain {
 
     // generates a new row and moves camera
     public void fly() {
-        calculateNewRow();
+        if ((cameraY % scl) == 0) {
+            calculateNewRow();
+        }
     }
 
     public void calculateNewRow() {
-        // create new array, with an additional row?
-        for(int x = 0; x < cols; x++) {
-            append(heightMap[x], 500);
+        int rowCount = heightMap[0].length; // amount of rows the heightMap array contains
+        int y = rowCount;
+        xoff = 0.0f;
+        for(int x = 0; x < cols; x++) { // traverse each col
+            // add new row (increase length by one for each col)
+            heightMap[x] = expand(heightMap[x], rowCount + 1);
+            heightMap[x][y] = map(noise(xoff, yoff), 0,1, 0, maxHeight);
+            xoff += 0.1f;
         }
-        
+        yoff += 0.1f;
+        rows += 1;
+
+        // TODO: if rows is bigger than threshold: move camera back and make a copy of the array only containing
+        // the current values
     }
 
     public void display() {
         stroke(255);
         //noFill();
-        fill(255, 20);
+        hint(DISABLE_DEPTH_TEST);
+        fill(255, 50);
 
         for (int y = 0; y < rows - 1; y++) {
             int widthOffset = 100;
@@ -128,6 +143,20 @@ class Terrain {
         }
     }
 }
+
+// class 2DArray {
+//     ArrayList<float[]> rows = new ArrayList<float[]>();
+
+//     2DArray(cols) {
+//         float[] cols = new float[cols];
+//     }
+
+//     // vals = coumn values for the new row
+//     void appendRow(float[] vals {
+//         rows.add(vals);
+//     }
+// }
+
   public void settings() {  size(800, 600, P3D); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "terrain" };
--- terrain/terrain.pde
+++ terrain/terrain.pde
@@ -5,13 +5,14 @@ Terrain t = new Terrain();
 float centerHeight = 100;
 float cameraAngle = 60.0;
 float shift = 0; // for moving the camera to fly over the terrain
+float cameraY = 0;
 
 void setup() {
     // create a 3D canvas
     size(800, 600, P3D);
     // camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
     // camera(width/2, height, centerZ + centerY / tan(cameraAngle * PI / 180), width/2, centerY, centerZ, 0, 1, 0);
-    camera(width/2, 0, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2), centerHeight, 0, 1, 0);
+    camera(width/2, cameraY, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2), centerHeight, 0, 1, 0);
     
 }
 
@@ -26,9 +27,9 @@ void draw() {
 }
 
 void moveCamera() {
-    shift += 0.5;
+    cameraY -= 2;
     //camera(width/2, height - shift, centerZ + centerY / tan(cameraAngle * PI / 180), width/2, centerY - shift, centerZ, 0, 1, 0);
-    camera(width/2, 0 - shift, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2) - shift, centerHeight, 0, 1, 0);
+    camera(width/2, cameraY, centerHeight + (height / 2) / tan(cameraAngle * PI / 180), width/2, (-height / 2) + cameraY, centerHeight, 0, 1, 0);
 }
 
 void positionCanvas() {
@@ -42,6 +43,9 @@ class Terrain {
     int w, h; // width and height - or better depth -  of the terrain
     int maxHeight; // defines the maximum height a mountain can have
     float[][] heightMap; // contains the height - or z value - for each vertice
+
+    float xoff = 0.0;
+    float yoff = 0.0;
 
     Terrain() {
         w = 1000;
@@ -58,9 +62,8 @@ class Terrain {
     }
 
     void calculateZValues() {
-        float yoff = 0.0;
         for (int y = 0; y < rows; y++) {
-            float xoff = 0.0; // set xoff to 0 before traversing a new row!
+            xoff = 0.0; // set xoff to 0 before traversing a new row!
             for (int x = 0; x < cols; x++) {
 
                 // set a z value for each element using perlin noise
@@ -74,21 +77,33 @@ class Terrain {
 
     // generates a new row and moves camera
     void fly() {
-        calculateNewRow();
+        if ((cameraY % scl) == 0) {
+            calculateNewRow();
+        }
     }
 
     void calculateNewRow() {
-        // create new array, with an additional row?
-        for(int x = 0; x < cols; x++) {
-            append(heightMap[x], 500);
+        int rowCount = heightMap[0].length; // amount of rows the heightMap array contains
+        int y = rowCount;
+        xoff = 0.0;
+        for(int x = 0; x < cols; x++) { // traverse each col
+            // add new row (increase length by one for each col)
+            heightMap[x] = expand(heightMap[x], rowCount + 1);
+            heightMap[x][y] = map(noise(xoff, yoff), 0,1, 0, maxHeight);
+            xoff += 0.1;
         }
-        
+        yoff += 0.1;
+        rows += 1;
+
+        // TODO: if rows is bigger than threshold: move camera back and make a copy of the array only containing
+        // the current values
     }
 
     void display() {
         stroke(255);
         //noFill();
-        fill(255, 20);
+        hint(DISABLE_DEPTH_TEST);
+        fill(255, 50);
 
         for (int y = 0; y < rows - 1; y++) {
             int widthOffset = 100;
@@ -112,3 +127,17 @@ class Terrain {
         }
     }
 }
+
+// class 2DArray {
+//     ArrayList<float[]> rows = new ArrayList<float[]>();
+
+//     2DArray(cols) {
+//         float[] cols = new float[cols];
+//     }
+
+//     // vals = coumn values for the new row
+//     void appendRow(float[] vals {
+//         rows.add(vals);
+//     }
+// }
+
